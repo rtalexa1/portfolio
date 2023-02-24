b04cf0e1d62360708e9f431b5271f804ff782a85
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-02-23 21:33:25 -0600

    Refactor App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <NavBar
-    @activate-dark-theme="activateDarkTheme"
-    @deactivate-dark-theme="deactivateDarkTheme"
-  />
+  <NavBar />
   <router-view />
-  <footer id="footer">
+  <footer
+    :class="{ 'footer-dark-theme': $store.state.darkThemeActive }"
+    id="footer"
+  >
     <span>Copyright 2023 RT Alexander</span> |
     <span>Contact: rtalexa1@gmail.com</span>
   </footer>
@@ -17,127 +17,127 @@ export default {
   components: {
     NavBar,
   },
-  methods: {
-    activateDarkTheme() {
-      const body = document.getElementsByTagName("body")[0];
-      body.classList.add("dark-theme");
-      this.activateAboutMeDarkTheme();
-      this.activateProjectsDarkTheme();
-      this.activateContactDarkTheme();
-      this.activateScreenshotsDarkTheme();
-      this.activateFooterDarkTheme();
-      localStorage.setItem("theme", "dark");
-    },
-    deactivateDarkTheme() {
-      const body = document.getElementsByTagName("body")[0];
-      body.classList.remove("dark-theme");
-      this.deactivateAboutMeDarkTheme();
-      this.deactivateProjectsDarkTheme();
-      this.deactivateContactDarkTheme();
-      this.deactivateScreenshotsDarkTheme();
-      this.deactivateFooterDarkTheme();
-      localStorage.setItem("theme", "light");
-    },
-    activateAboutMeDarkTheme() {
-      const photoContainer = document.getElementById("photo-container");
-      photoContainer.style.borderColor = "#376767";
-      photoContainer.style.backgroundColor = "#376767";
+  // methods: {
+  //   activateDarkTheme() {
+  //     const body = document.getElementsByTagName("body")[0];
+  //     body.classList.add("dark-theme");
+  //     this.activateAboutMeDarkTheme();
+  //     this.activateProjectsDarkTheme();
+  //     this.activateContactDarkTheme();
+  //     this.activateScreenshotsDarkTheme();
+  //     this.activateFooterDarkTheme();
+  //     localStorage.setItem("theme", "dark");
+  //   },
+  //   deactivateDarkTheme() {
+  //     const body = document.getElementsByTagName("body")[0];
+  //     body.classList.remove("dark-theme");
+  //     this.deactivateAboutMeDarkTheme();
+  //     this.deactivateProjectsDarkTheme();
+  //     this.deactivateContactDarkTheme();
+  //     this.deactivateScreenshotsDarkTheme();
+  //     this.deactivateFooterDarkTheme();
+  //     localStorage.setItem("theme", "light");
+  //   },
+  //   activateAboutMeDarkTheme() {
+  //     const photoContainer = document.getElementById("photo-container");
+  //     photoContainer.style.borderColor = "#376767";
+  //     photoContainer.style.backgroundColor = "#376767";
 
-      const aboutMeText = document.getElementById("about-me-text");
-      aboutMeText.classList.add("text-dark-theme");
-    },
-    deactivateAboutMeDarkTheme() {
-      const photoContainer = document.getElementById("photo-container");
-      photoContainer.style.borderColor = "#63b7b7";
-      photoContainer.style.backgroundColor = "#63b7b7";
+  //     const aboutMeText = document.getElementById("about-me-text");
+  //     aboutMeText.classList.add("text-dark-theme");
+  //   },
+  //   deactivateAboutMeDarkTheme() {
+  //     const photoContainer = document.getElementById("photo-container");
+  //     photoContainer.style.borderColor = "#63b7b7";
+  //     photoContainer.style.backgroundColor = "#63b7b7";
 
-      const aboutMeText = document.getElementById("about-me-text");
-      aboutMeText.classList.remove("text-dark-theme");
-    },
-    activateProjectsDarkTheme() {
-      const title = document.getElementById("projects-title");
-      title.style.color = "white";
-      title.style.filter = "drop-shadow(0 0 2px #faf9f6)";
+  //     const aboutMeText = document.getElementById("about-me-text");
+  //     aboutMeText.classList.remove("text-dark-theme");
+  //   },
+  //   activateProjectsDarkTheme() {
+  //     const title = document.getElementById("projects-title");
+  //     title.style.color = "white";
+  //     title.style.filter = "drop-shadow(0 0 2px #faf9f6)";
 
-      const titles = document.getElementsByClassName("project-title");
-      for (let i = 0; i < titles.length; i++) {
-        titles[i].classList.add("text-dark-theme");
-        // To-do: Add text shadow to h2's
-        // titles[i].innerText.style.filter = "drop-shadow(0 0 2px #faf9f6)";
-      }
+  //     const titles = document.getElementsByClassName("project-title");
+  //     for (let i = 0; i < titles.length; i++) {
+  //       titles[i].classList.add("text-dark-theme");
+  //       // To-do: Add text shadow to h2's
+  //       // titles[i].innerText.style.filter = "drop-shadow(0 0 2px #faf9f6)";
+  //     }
 
-      const synopses = document.getElementsByClassName("project-synopsis");
-      for (let i = 0; i < synopses.length; i++) {
-        synopses[i].classList.add("text-dark-theme");
-      }
-    },
-    deactivateProjectsDarkTheme() {
-      const title = document.getElementById("projects-title");
-      title.style.color = "#63b7b7";
+  //     const synopses = document.getElementsByClassName("project-synopsis");
+  //     for (let i = 0; i < synopses.length; i++) {
+  //       synopses[i].classList.add("text-dark-theme");
+  //     }
+  //   },
+  //   deactivateProjectsDarkTheme() {
+  //     const title = document.getElementById("projects-title");
+  //     title.style.color = "#63b7b7";
 
-      const titles = document.getElementsByClassName("project-title");
-      for (let i = 0; i < titles.length; i++) {
-        titles[i].classList.remove("text-dark-theme");
-      }
+  //     const titles = document.getElementsByClassName("project-title");
+  //     for (let i = 0; i < titles.length; i++) {
+  //       titles[i].classList.remove("text-dark-theme");
+  //     }
 
-      const synopses = document.getElementsByClassName("project-synopsis");
-      for (let i = 0; i < synopses.length; i++) {
-        synopses[i].classList.remove("text-dark-theme");
-      }
-    },
-    activateContactDarkTheme() {
-      const contactForm = document.getElementById("contact-form");
-      contactForm.classList.add("contact-form-dark-theme");
-    },
-    deactivateContactDarkTheme() {
-      const contactForm = document.getElementById("contact-form");
-      contactForm.classList.remove("contact-form-dark-theme");
-    },
-    activateScreenshotsDarkTheme() {
-      const exitTicketScreenshot = document.getElementById(
-        "exit-ticket-screenshot"
-      );
-      exitTicketScreenshot.classList.add("exit-ticket-screenshot-dark-theme");
+  //     const synopses = document.getElementsByClassName("project-synopsis");
+  //     for (let i = 0; i < synopses.length; i++) {
+  //       synopses[i].classList.remove("text-dark-theme");
+  //     }
+  //   },
+  //   activateContactDarkTheme() {
+  //     const contactForm = document.getElementById("contact-form");
+  //     contactForm.classList.add("contact-form-dark-theme");
+  //   },
+  //   deactivateContactDarkTheme() {
+  //     const contactForm = document.getElementById("contact-form");
+  //     contactForm.classList.remove("contact-form-dark-theme");
+  //   },
+  //   activateScreenshotsDarkTheme() {
+  //     const exitTicketScreenshot = document.getElementById(
+  //       "exit-ticket-screenshot"
+  //     );
+  //     exitTicketScreenshot.classList.add("exit-ticket-screenshot-dark-theme");
 
-      const mathVizScreenshot = document.getElementById("math-viz-screenshot");
-      mathVizScreenshot.classList.add("math-viz-screenshot-dark-theme");
+  //     const mathVizScreenshot = document.getElementById("math-viz-screenshot");
+  //     mathVizScreenshot.classList.add("math-viz-screenshot-dark-theme");
 
-      const divyUpScreenshot = document.getElementById("divy-up-screenshot");
-      divyUpScreenshot.classList.add("divy-up-screenshot-dark-theme");
-    },
-    deactivateScreenshotsDarkTheme() {
-      const exitTicketScreenshot = document.getElementById(
-        "exit-ticket-screenshot"
-      );
-      exitTicketScreenshot.classList.remove(
-        "exit-ticket-screenshot-dark-theme"
-      );
+  //     const divyUpScreenshot = document.getElementById("divy-up-screenshot");
+  //     divyUpScreenshot.classList.add("divy-up-screenshot-dark-theme");
+  //   },
+  //   deactivateScreenshotsDarkTheme() {
+  //     const exitTicketScreenshot = document.getElementById(
+  //       "exit-ticket-screenshot"
+  //     );
+  //     exitTicketScreenshot.classList.remove(
+  //       "exit-ticket-screenshot-dark-theme"
+  //     );
 
-      const mathVizScreenshot = document.getElementById("math-viz-screenshot");
-      mathVizScreenshot.classList.remove("math-viz-screenshot-dark-theme");
+  //     const mathVizScreenshot = document.getElementById("math-viz-screenshot");
+  //     mathVizScreenshot.classList.remove("math-viz-screenshot-dark-theme");
 
-      const divyUpScreenshot = document.getElementById("divy-up-screenshot");
-      divyUpScreenshot.classList.remove("divy-up-screenshot-dark-theme");
-    },
-    activateFooterDarkTheme() {
-      const footer = document.getElementById("footer");
-      footer.style.background = "#1b1212";
-      footer.style.color = "#faf9f6";
-    },
-    deactivateFooterDarkTheme() {
-      const footer = document.getElementById("footer");
-      footer.style.background = "#faf9f6";
-      footer.style.color = "#1b1212";
-    },
-  },
-  updated() {
-    if (localStorage.theme === undefined) {
-      localStorage.setItem("theme", "light");
-      this.deactivateDarkTheme();
-    } else if (localStorage.theme === "dark") {
-      this.activateDarkTheme();
-    }
-  },
+  //     const divyUpScreenshot = document.getElementById("divy-up-screenshot");
+  //     divyUpScreenshot.classList.remove("divy-up-screenshot-dark-theme");
+  //   },
+  //   activateFooterDarkTheme() {
+  //     const footer = document.getElementById("footer");
+  //     footer.style.background = "#1b1212";
+  //     footer.style.color = "#faf9f6";
+  //   },
+  //   deactivateFooterDarkTheme() {
+  //     const footer = document.getElementById("footer");
+  //     footer.style.background = "#faf9f6";
+  //     footer.style.color = "#1b1212";
+  //   },
+  // },
+  // updated() {
+  //   if (localStorage.theme === undefined) {
+  //     localStorage.setItem("theme", "light");
+  //     this.deactivateDarkTheme();
+  //   } else if (localStorage.theme === "dark") {
+  //     this.activateDarkTheme();
+  //   }
+  // },
 };
 </script>
 
@@ -185,6 +185,11 @@ footer {
   background: #faf9f6;
   font-size: medium;
   text-align: center;
+}
+
+.footer-dark-theme {
+  background: #1b1212;
+  color: #faf9f6;
 }
 
 @media (max-width: 768px) {
